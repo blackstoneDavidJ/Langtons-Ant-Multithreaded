35fe210a4940d475dba7e15bd795cd658fae0c18
David Blackstone <62959991+blackstoneDavidJ@users.noreply.github.com>
2022-06-29 22:10:11 -0400

    Update Main.java
    UI updates aswell as bug fixes

--- Main.java
+++ Main.java
@@ -5,12 +5,14 @@ import java.awt.Graphics2D;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.util.ArrayList;
 import java.util.Random;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -25,18 +27,23 @@ public class Main extends JFrame
 	private static int antNum = 0;
 	private static long antSteps = 0;
 	private static boolean sleep = false;
+	private static boolean paused = false;
+	private static ArrayList<Langton> antList;
 	private static Dimension screen; 
+	private static JFrame mainFrame;
+	private static JPanel drawPanel, antPanel;
 	
 	public static void main(String[] args)
 	{
-		JFrame mainFrame = new JFrame();
-		JPanel antPanel = new JPanel(new BorderLayout());
-		JPanel drawPanel = new JPanel();
+		mainFrame = new JFrame();
+		antPanel = new JPanel(new BorderLayout());
+		drawPanel = new JPanel();
 		mainFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		screen = Toolkit.getDefaultToolkit().getScreenSize();
-		mainFrame.setSize(screen.width, screen.height);
-		drawPanel.setSize(screen.width, screen.height);
-		drawPanel.setBackground(Color.black);
+		drawPanel.setSize(screen.width, screen.height);		
+		drawPanel.setBackground(Color.white);
+		antList = new ArrayList<>();
 		
 		JTextField antNumText = new JTextField("1");	
 		JTextField antStepsText = new JTextField("11000");
@@ -44,9 +51,21 @@ public class Main extends JFrame
 		JLabel antStepsLabel = new JLabel("Amount of Ant Steps");
 		JButton runButton = new JButton(new ImageIcon(Main.class.getResource("resources/run.png")));
 		JButton stopButton = new JButton(new ImageIcon(Main.class.getResource("resources/stop.png")));
+		JButton clearButton = new JButton(new ImageIcon(Main.class.getResource("resources/clear.png")));
+		JButton colorButton = new JButton(new ImageIcon(Main.class.getResource("resources/color.png")));
 		JCheckBox isSleep = new JCheckBox("Enable Sleep");
+		isSleep.setToolTipText("Move slowly through simulation");
 		JToolBar toolBar = new JToolBar();
 		
+		mainFrame.setTitle("Langton's Ant by David Blackstone");
+		mainFrame.addComponentListener(new ComponentAdapter() {
+			public void componentResized(ComponentEvent componentEvent)
+			{
+				//window needs resizing support!!!!!
+			}
+		});
+		
+		runButton.setToolTipText("Run the Simulation");
 		runButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
@@ -54,26 +73,65 @@ public class Main extends JFrame
 				antSteps = Long.valueOf(antStepsText.getText());
 				if(isSleep.isSelected()) sleep = true;
 				else sleep = false;
-				try 
-				{	
-					runSimulation((Graphics2D) drawPanel.getGraphics());
-				} 
-				
-				catch (InterruptedException e1) 
+
+				new Thread(new Runnable() {
+					@Override
+					public void run()
+					{
+						try 
+						{
+							if(!paused)
+							{
+								runSimulation((Graphics2D) drawPanel.getGraphics());
+							}
+							
+							else
+							{
+								for(Langton lang : antList)
+								{
+									lang.start();
+								}
+							}
+						} 
+						
+						catch (InterruptedException e) 
+						{
+							e.printStackTrace();
+						}
+					}
+				}).start();
+			}
+		});
+		
+		stopButton.setToolTipText("Stop the Simulation");
+		stopButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				for(Langton lang : antList)
 				{
-					e1.printStackTrace();
+					lang.stop();
 				}
 			}
 		});
 		
-		stopButton.addActionListener(new ActionListener() {
+		clearButton.setToolTipText("Clear the screen");
+		clearButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
 				antNum = 0;
 				antSteps = 0;
-				antPanel.invalidate();
-				antPanel.validate();
-				antPanel.repaint();
+				paused = false;
+				drawPanel.invalidate();
+				drawPanel.validate();
+				drawPanel.repaint();	
+			}
+		});
+		
+		colorButton.setToolTipText("set Background Color");
+		colorButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				drawPanel.setBackground(JColorChooser.showDialog(mainFrame, "Select Background Color", Color.white));
 			}
 		});
 		
@@ -81,9 +139,11 @@ public class Main extends JFrame
 		toolBar.add(antStepsText);
 		toolBar.add(antNumLabel);
 		toolBar.add(antNumText);
+		toolBar.add(colorButton);
 		toolBar.add(isSleep);
 		toolBar.add(runButton);
 		toolBar.add(stopButton);
+		toolBar.add(clearButton);
 		antPanel.add(toolBar, BorderLayout.PAGE_START);
 		antPanel.add(drawPanel, BorderLayout.CENTER);
 		
@@ -94,7 +154,6 @@ public class Main extends JFrame
 	private static void runSimulation(Graphics2D g2d) throws InterruptedException
 	{
 		Random rand = new Random();
-		ArrayList<Thread> antList = new ArrayList<>();
 		
 		for(int i = 0; i < antNum; i++)
 		{
@@ -110,15 +169,8 @@ public class Main extends JFrame
 				sleep
 			);	
 			
-			Thread thread = new Thread(lang);
-			thread.start();
-			antList.add(thread);
-		}
-		
-		for(int i = 0; i < antList.size(); i++)
-		{
-			Thread tmp = antList.get(i);
-			tmp.join();
+			lang.start();
+			antList.add(lang);
 		}
 	}
 }
